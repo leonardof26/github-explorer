ec0d22c3b362a71141675c58a53974e70f70b9e7
leonardopf26 <leonardopf26@gmail.com>
2021-04-03 14:16:16 -0300

    Added and configured webpack live server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: { contentBase: path.resolve(__dirname, "public") },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
